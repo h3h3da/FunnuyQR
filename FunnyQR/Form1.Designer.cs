f582270d6d97e12926fd566162b7a1bf0b465bfb
heheda <1162858612@qq.com>
2017-06-11 03:05:54 -0500

    Add files via upload

---
+++ FunnyQR/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace FunnyQR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.backpic = new System.Windows.Forms.PictureBox();
+            this.qrpic = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbox = new System.Windows.Forms.RichTextBox();
+            this.picselect = new System.Windows.Forms.Button();
+            this.qrout = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.backp = new System.Windows.Forms.Label();
+            this.qrp = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.backpic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qrpic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // backpic
+            // 
+            this.backpic.Location = new System.Drawing.Point(12, 218);
+            this.backpic.Name = "backpic";
+            this.backpic.Size = new System.Drawing.Size(280, 280);
+            this.backpic.TabIndex = 0;
+            this.backpic.TabStop = false;
+            // 
+            // qrpic
+            // 
+            this.qrpic.Location = new System.Drawing.Point(412, 218);
+            this.qrpic.Name = "qrpic";
+            this.qrpic.Size = new System.Drawing.Size(280, 280);
+            this.qrpic.TabIndex = 1;
+            this.qrpic.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "请输入文本信息： ";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Font = new System.Drawing.Font("Segoe Marker", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(230, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(306, 40);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Welcome To FunnyQR!";
+            // 
+            // txtbox
+            // 
+            this.txtbox.Location = new System.Drawing.Point(12, 81);
+            this.txtbox.Name = "txtbox";
+            this.txtbox.Size = new System.Drawing.Size(680, 96);
+            this.txtbox.TabIndex = 4;
+            this.txtbox.Text = "";
+            // 
+            // picselect
+            // 
+            this.picselect.Location = new System.Drawing.Point(216, 189);
+            this.picselect.Name = "picselect";
+            this.picselect.Size = new System.Drawing.Size(75, 23);
+            this.picselect.TabIndex = 6;
+            this.picselect.Text = "选  择";
+            this.picselect.UseVisualStyleBackColor = true;
+            this.picselect.Click += new System.EventHandler(this.picinput_Click);
+            // 
+            // qrout
+            // 
+            this.qrout.Location = new System.Drawing.Point(615, 189);
+            this.qrout.Name = "qrout";
+            this.qrout.Size = new System.Drawing.Size(75, 23);
+            this.qrout.TabIndex = 7;
+            this.qrout.Text = "一键生成";
+            this.qrout.UseVisualStyleBackColor = true;
+            this.qrout.Click += new System.EventHandler(this.qrout_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "插入背景：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(412, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "二维码：";
+            // 
+            // backp
+            // 
+            this.backp.AutoSize = true;
+            this.backp.Location = new System.Drawing.Point(10, 501);
+            this.backp.Name = "backp";
+            this.backp.Size = new System.Drawing.Size(65, 12);
+            this.backp.TabIndex = 10;
+            this.backp.Text = "图片路径：";
+            // 
+            // qrp
+            // 
+            this.qrp.AutoSize = true;
+            this.qrp.Location = new System.Drawing.Point(327, 504);
+            this.qrp.Name = "qrp";
+            this.qrp.Size = new System.Drawing.Size(65, 12);
+            this.qrp.TabIndex = 11;
+            this.qrp.Text = "保存路径：";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(588, 54);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(113, 12);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Version 1.2.2 beta";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(327, 66);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(35, 12);
+            this.linkLabel1.TabIndex = 13;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "About";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(316, 257);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(60, 16);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "黑  白";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(316, 302);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(60, 16);
+            this.checkBox2.TabIndex = 15;
+            this.checkBox2.Text = "彩  色";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(314, 382);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(65, 12);
+            this.linkLabel2.TabIndex = 16;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "linkLabel2";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Location = new System.Drawing.Point(314, 354);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(65, 12);
+            this.linkLabel3.TabIndex = 17;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "linkLabel3";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 525);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.qrp);
+            this.Controls.Add(this.backp);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.qrout);
+            this.Controls.Add(this.picselect);
+            this.Controls.Add(this.txtbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.qrpic);
+            this.Controls.Add(this.backpic);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "FunnyQR";
+            ((System.ComponentModel.ISupportInitialize)(this.backpic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qrpic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox backpic;
+        private System.Windows.Forms.PictureBox qrpic;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox txtbox;
+        private System.Windows.Forms.Button picselect;
+        private System.Windows.Forms.Button qrout;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label backp;
+        private System.Windows.Forms.Label qrp;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+    }
+}
+
---
+++ FunnyQR/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FunnyQR
+{
+    public partial class Form1 : Form
+    {
+        public string picname;
+        public string qrpath;
+        public Form1()
+        {
+            InitializeComponent();
+            linkLabel2.Hide();
+            linkLabel3.Hide();
+        }
+
+        private void txtinput_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("For No.1 test!");
+        }
+
+        private void picinput_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "选择要插入的图片";
+            ofd.InitialDirectory = @"C:\Users\Administrator\Desktop";
+            ofd.Filter = "|*.png||*.jpg||*.jpeg||*.bmp||*.gif||*.svg";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string path = ofd.FileName;//你选中的图片的绝对路径
+                Image tempic;
+                tempic = Image.FromFile(path);
+                backpic.Image = AdjImageToFitSize(tempic,backpic.Width, backpic.Height);
+                //backpic.Image = Image.FromFile(path);
+               // MessageBox.Show(path);
+                picname = System.IO.Path.GetFileNameWithoutExtension(ofd.FileName);
+                qrpath = System.IO.Path.GetDirectoryName(ofd.FileName);
+            }
+            //ofd.ShowDialog();
+            //得到选择文件的路径
+            
+            backp.Text = ofd.FileName;
+          //  linkLabel2.Show();
+            
+        }
+        public static Image AdjImageToFitSize(Image fromImage, int width, int height)
+        {
+            Bitmap bitmap = new Bitmap(width, height);
+
+            Graphics graphics = Graphics.FromImage(bitmap);
+            Point[] destPoints = new Point[] {  
+                new Point(0, 0),  
+                new Point(width, 0),  
+                new Point(0, height)  
+            };
+
+            Rectangle rect = GetImageRectangle(fromImage.Width, fromImage.Height);
+            graphics.DrawImage(fromImage, destPoints, rect, GraphicsUnit.Pixel);
+
+            Image image = Image.FromHbitmap(bitmap.GetHbitmap());
+            bitmap.Dispose();
+            graphics.Dispose();
+            return image;
+        }
+        public static Rectangle GetImageRectangle(int w, int h)
+        {
+            int x = 0;
+            int y = 0;
+
+            if (h > w)
+            {
+                h = w;
+                y = (h - w) / 2;
+            }
+            else
+            {
+                w = h;
+                x = (w - h) / 2;
+            }
+
+            return new Rectangle(x, y, w, h);
+        }  
+
+        private void qrout_Click(object sender, EventArgs e)
+        {
+
+            if (txtbox.Text == "")
+            {
+                MessageBox.Show("请输入文本信息！");
+            }
+            else
+            {
+               Image myimage;
+                if (checkBox1.CheckState == CheckState.Unchecked && checkBox2.CheckState == CheckState.Unchecked)
+                {
+                    MessageBox.Show("请选择二维码颜色！");
+                }
+                else if (checkBox1.CheckState == CheckState.Checked && checkBox2.CheckState == CheckState.Checked)
+                {
+                    MessageBox.Show("请选择一种二维码颜色！");
+                }
+                else if (checkBox1.CheckState == CheckState.Checked && checkBox2.CheckState == CheckState.Unchecked)
+                {
+                    string str = "myqr " + txtbox.Text + " -p " + backp.Text + " -n " + qrpath + @"\" + picname + "_qrcode.png -v 5 -l H";
+
+                    System.Diagnostics.Process p = new System.Diagnostics.Process();
+                    p.StartInfo.FileName = "cmd.exe";
+                    p.StartInfo.UseShellExecute = false;    //是否使用操作系统shell启动
+                    p.StartInfo.RedirectStandardInput = true;//接受来自调用程序的输入信息
+                    p.StartInfo.RedirectStandardOutput = true;//由调用程序获取输出信息
+                    p.StartInfo.RedirectStandardError = true;//重定向标准错误输出
+                    p.StartInfo.CreateNoWindow = true;//不显示程序窗口
+                    p.Start();//启动程序
+
+                    //向cmd窗口发送输入信息
+                    p.StandardInput.WriteLine(str + "&exit");
+
+                    p.StandardInput.AutoFlush = true;
+                    //p.StandardInput.WriteLine("exit");
+                    //向标准输入写入要执行的命令。这里使用&是批处理命令的符号，表示前面一个命令不管是否执行成功都执行后面(exit)命令，如果不执行exit命令，后面调用ReadToEnd()方法会假死
+                    //同类的符号还有&&和||前者表示必须前一个命令执行成功才会执行后面的命令，后者表示必须前一个命令执行失败才会执行后面的命令
+
+
+
+                    //获取cmd窗口的输出信息
+                   // string output = p.StandardOutput.ReadToEnd();
+
+                    //txtbox.Text = output;
+
+                    p.WaitForExit();//等待程序执行完退出进程
+                    p.Close();
+                   // qrpath = Environment.CurrentDirectory;
+                   // MessageBox.Show(qrpath + @"\" + picname + "_qrcode.png");
+                    myimage = Image.FromFile(qrpath + @"\" + picname + "_qrcode.png");  
+                    qrpic.Image = AdjImageToFitSize(myimage,qrpic.Width, qrpic.Height); //350  
+                    qrp.Text = qrpath + @"\" + picname + "_qrcode.png";
+                    //linkLabel3.Show();
+                    
+                     
+                    
+                    //qrpic.Image
+                }
+                else if (checkBox1.CheckState == CheckState.Unchecked && checkBox2.CheckState == CheckState.Checked)
+                {
+                    string str = "myqr " + txtbox.Text + " -p " + backp.Text + " -n " + qrpath + @"\c_" + picname + "_qrcode.png -c -v 5 -l H";
+
+                    System.Diagnostics.Process p = new System.Diagnostics.Process();
+                    p.StartInfo.FileName = "cmd.exe";
+                    p.StartInfo.UseShellExecute = false;    //是否使用操作系统shell启动
+                    p.StartInfo.RedirectStandardInput = true;//接受来自调用程序的输入信息
+                    p.StartInfo.RedirectStandardOutput = true;//由调用程序获取输出信息
+                    p.StartInfo.RedirectStandardError = true;//重定向标准错误输出
+                    p.StartInfo.CreateNoWindow = true;//不显示程序窗口
+                    p.Start();//启动程序
+
+                    //向cmd窗口发送输入信息
+                    p.StandardInput.WriteLine(str + "&exit");
+
+                    p.StandardInput.AutoFlush = true;
+                    //p.StandardInput.WriteLine("exit");
+                    //向标准输入写入要执行的命令。这里使用&是批处理命令的符号，表示前面一个命令不管是否执行成功都执行后面(exit)命令，如果不执行exit命令，后面调用ReadToEnd()方法会假死
+                    //同类的符号还有&&和||前者表示必须前一个命令执行成功才会执行后面的命令，后者表示必须前一个命令执行失败才会执行后面的命令
+
+
+
+                    //获取cmd窗口的输出信息
+                    //string output = p.StandardOutput.ReadToEnd();
+
+                    //txtbox.Text = output;
+
+                    p.WaitForExit();//等待程序执行完退出进程
+                    p.Close();
+                    //qrpath = Environment.CurrentDirectory;
+                    //MessageBox.Show(picname);
+                   // MessageBox.Show(qrpath + @"\c_" + picname + "_qrcode.png");
+                    myimage = Image.FromFile(qrpath + @"\c_" + picname + "_qrcode.png");
+                    qrpic.Image = AdjImageToFitSize(myimage, qrpic.Width, qrpic.Height); //350  
+                    qrp.Text = qrpath + @"\c_" + picname + "_qrcode.png";
+                    //linkLabel3.Show();
+                    
+                }
+                
+            }
+            
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.linkLabel1.Links[0].LinkData = "https://github.com/h3h3da/qrcode";
+            System.Diagnostics.Process.Start(e.Link.LinkData.ToString());
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            //this.linkLabel1.Links[0].LinkData = "file://" + backpicpath.Text;
+          //  MessageBox.Show("file://" + backpicpath.Text);
+          //  System.Diagnostics.Process.Start(e.Link.LinkData.ToString());
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+           // this.linkLabel1.Links[0].LinkData = "file://" + qrpicpath.Text;
+            //MessageBox.Show("file://" + qrpicpath.Text);
+           // System.Diagnostics.Process.Start(e.Link.LinkData.ToString());
+        }
+        
+    }
+}
